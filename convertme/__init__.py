24bb586f1855a92eb7d78ade415a56255c6cf114
Tomáš Mikula <mail@tomasmikula.cz>
2020-03-23 21:38:53 +0100

    Fix broken setup.py

--- convertme/__init__.py
+++ convertme/__init__.py
@@ -7,7 +7,7 @@ from .csv.csv_reader import CsvReader
 from .csv.csv_writer import CsvWriter
 from .burmeister.burmeister_reader import BurmeisterReader
 
-__authors__ = 'Radek Janoštík, Tomáš Mikula, Roman Vyjídáček'
+__author__ = 'Radek Janoštík, Tomáš Mikula, Roman Vyjídáček'
 __email__ = 'radek.janostik@upol.cz, mail@tomasmikula.cz, r.vyjidacek@gmail.com'
 __version__ = '0.1.0'
 __license__ = 'MIT license'

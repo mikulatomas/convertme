73e58b1f4b56008c76e1b396e30cd87fb6b78e42
Tomáš Mikula <mail@tomasmikula.cz>
2020-03-23 19:20:34 +0100

    New structure of code

--- convertme/__init__.py
+++ convertme/__init__.py
@@ -1,10 +1,10 @@
 """Top-level package for ConvertMe."""
 
 from .dataset import Dataset
-from .reader import ReaderInterface
-from .writer import WriterInterface
-from .csv_reader import CsvReader
-from .csv_writer import CsvWriter
+from .reader_interface import ReaderInterface
+from .writer_interface import WriterInterface
+from .csv.csv_reader import CsvReader
+from .csv.csv_writer import CsvWriter
 
 __author__ = """Tomáš Mikula"""
 __email__ = 'mail@tomasmikula.cz'

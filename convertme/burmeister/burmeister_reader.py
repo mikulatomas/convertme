5ebe632c5c4b416537d1bed5651ce9b5afb158da
Tomáš Mikula <mail@tomasmikula.cz>
2020-03-24 13:10:59 +0100

    Clean code 🛀

--- convertme/burmeister/burmeister_reader.py
+++ convertme/burmeister/burmeister_reader.py
@@ -1,29 +1,30 @@
 from convertme import ReaderInterface, Dataset
 
 
-
 class BurmeisterReader(ReaderInterface):
 
     def __process_char(self, char):
-        return char=='X' or char=='x'
+        return char == 'X' or char == 'x'
 
     def read(self, file):
-        # default Python csv reader
         bools = []
         lines = file.read().splitlines()
-        if lines[0]!="B":
+
+        if lines[0] != "B":
             raise ValueError("First line has to be \"B\"")
-        
+
         obj_count = int(lines[2])
         att_count = int(lines[3])
         objects = []
         attributes = []
+
         for i in range(0, obj_count):
             objects.append(lines[4 + i])
+
         for i in range(0, att_count):
             attributes.append(lines[4 + obj_count + i])
+
         for i in range(obj_count + att_count + 4, len(lines)):
             bools.append(list(map(self.__process_char, lines[i])))
-
 
         return Dataset(objects, attributes, bools)
--- convertme/burmeister/burmeister_writer.py
+++ convertme/burmeister/burmeister_writer.py
@@ -1,7 +1,6 @@
 from convertme import WriterInterface, Dataset
 
 
-
 class BurmeisterWriter(WriterInterface):
     def __process_val(self, b):
         return "X" if b else "."
@@ -10,14 +9,18 @@ class BurmeisterWriter(WriterInterface):
         for s in lst:
             output.write(s)
             output.write("\n")
-    
+
     def write(self, dataset, output):
-        content = ["B", "", str(len(dataset.objects)), str(len(dataset.attributes))]
+        content = ["B", "", str(len(dataset.objects)),
+                   str(len(dataset.attributes))]
+
         self.__write_list(content, output)
         self.__write_list(dataset.objects, output)
         self.__write_list(dataset.attributes, output)
-        data=[]
-        for idx,row in enumerate(dataset.bools):
-            data.append(''.join(list(map(self.__process_val, row))))
-        self.__write_list(data, output)
 
+        data = []
+
+        for row in dataset.bools:
+            data.append(''.join(list(map(self.__process_val, row))))
+
+        self.__write_list(data, output)

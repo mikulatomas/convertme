6f22b3104a660e000a584c2f8e5f66de5f5c25c0
Tomas Urbanec <tomas.urbanec@upol.cz>
2020-04-03 14:35:03 +0200

    Basic cex reader.

---
+++ convertme/cex/cex_reader.py
@@ -0,0 +1,55 @@
+from convertme import ReaderInterface, Dataset
+from xml.etree.ElementTree import parse
+
+class CexReader(ReaderInterface):
+
+    # TODO add file validation ?
+    def read(self, file) -> Dataset:
+        tree = parse(file)
+        root = tree.getroot()
+        context = root.find('Contexts').find('Context')
+        
+        # TODO if we are going to us only order of attributes (ie not their identifiers),
+        # then simple list can be used instead of dict (filled in order,
+        #  indexed by object's hasAttribute identifiers)
+
+        # read attributes
+        xml_attributes = context.find('Attributes')
+        attributes = []
+        # identifer-name dictionary for attributes
+        attrib_dict = {}
+        for attr in xml_attributes.findall('Attribute'):
+            name = attr.find('Name').text
+            attributes.append(name)
+            identifier = attr.get('Identifier')
+            attrib_dict.update({identifier: name})
+
+        # incidency
+        bools = []
+        # read objects
+        xml_objects = context.find('Objects')
+        objects = []
+        for obj in xml_objects.findall('Object'):
+            name = obj.find('Name').text
+            objects.append(name)
+            xml_int =  obj.find('Intent')
+            intent = []
+            # read intent
+            for attr in xml_int:
+                intent.append(attrib_dict[attr.get('AttributeIdentifier')])
+            bools.append([attrib in intent for attrib in attributes])
+        
+        # TODO can contain more contexts?
+        # TODO reads in order from xml file, but order can be changed
+        # should attributes order be given by identifiers or by order in file?
+        # concept-explorer ignores the identifiers and uses order in file instead
+        # therefore use order for both objects and attributes?
+
+        # Moreover cex does not have to contain <RecalculationPolicy Value="Clear" />
+        # <Lattices /> and identifiers for contexts and attributes
+        # still might put them in the writer in this empty form 
+        # - at least conexp 1.5 notices if <Lattices /> is missing.
+        # It seems that only important part for us is content of <attributes> and <objects> 
+        # I am still not sure how (or even if) identifiers are used.
+        # TODO cex can also contain lattice description... ignore?
+        return Dataset(objects,attributes,bools,"")

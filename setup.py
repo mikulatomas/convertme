4ed5a31fc5d227d8ca384aedf2bc02156dd8d125
Tomáš Mikula <mail@tomasmikula.cz>
2020-04-07 11:28:32 +0200

    Required Python version 3.4

--- setup.py
+++ setup.py
@@ -25,13 +25,16 @@ test_requirements = ['pytest>=3', ]
 setup(
     author=__author__,
     author_email=__email__,
-    python_requires='>=3.7',
+    python_requires='>=3.4',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
